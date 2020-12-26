cad556b50e29b8cbf1f18793f6e5127a4267169e
luciofarinosi <Lucio.Farinosi@ie.ey.com>
2020-12-26 12:18:23 +0000

    update menu-item for managing routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 
+import {Switch, Route } from 'react-router-dom';
+
 import { HomePage } from './pages/homepage/homepage.component';
 
 import './App.css';
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
+
 function App() {
   return (
-    <HomePage/>
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route exact path='/hats' component={HatsPage}/>
+      </Switch>
+    </div>
   );
 }
 
